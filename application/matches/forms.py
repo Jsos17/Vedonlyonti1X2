09b845bf9297c204eeb8b9a1f30597ae7ffa30a0
Juuso Silvennoinen <32332926+Jsos17@users.noreply.github.com>
2018-05-28 18:28:58 +0300

    käyttäjätarinat, readme päivitys

--- application/matches/forms.py
+++ application/matches/forms.py
@@ -1,26 +1,24 @@
 from flask_wtf import FlaskForm
-from wtforms import (DateTimeField, IntegerField,
-                     StringField, validators)
+from wtforms import DateTimeField, IntegerField, StringField, validators
 
 def validate_probabilities(form, field):
-    p1 = form.prob1.data
-    px = form.probx.data
-    p2 = form.prob2.data
     try:
+        p1 = int(form.prob1.data)
+        px = int(form.probx.data)
+        p2 = int(form.prob2.data)
+
         if p1 + px + p2 != 100:
             raise validators.ValidationError("The sum of probabilities is not exactly 100 %")
     except TypeError:
-        raise validators.ValidationError("Not a number")
-    except ValueError:
-        raise validators.ValidationError("Not a number")
-
+        raise validators.ValidationError()
+    
 class MatchForm(FlaskForm):
     home = StringField("Home team", [validators.InputRequired()])
     away = StringField("Away team", [validators.InputRequired()])
-    prob1 = IntegerField("Home win probability (%)", [validators.NumberRange(min=0, max=100, message="Number between 0-100"), validate_probabilities])
-    probx = IntegerField("Draw probability (%)", [validators.NumberRange(min=0, max=100, message="Number between 0-100"), validate_probabilities])
-    prob2 = IntegerField("Away win probability (%)", [validators.NumberRange(min=0, max=100, message="Number between 0-100"), validate_probabilities])
-    start_time = DateTimeField("Starting time (YYYY-mm-dd HH:MM)", format='%Y-%m-%d %H:%M')
+    prob1 = IntegerField("Home win probability (integer value 0-100 %)", [validators.NumberRange(min=0, max=100), validate_probabilities])
+    probx = IntegerField("Draw probability (integer value 0-100 %)", [validators.NumberRange(min=0, max=100), validate_probabilities])
+    prob2 = IntegerField("Away win probability (integer value 0-100 %)", [validators.NumberRange(min=0, max=100), validate_probabilities])
+    start_time = DateTimeField("Starting time (format: YYYY-mm-dd HH:MM)", format='%Y-%m-%d %H:%M')
  
     class Meta:
         csrf = False
---
+++ documentation/user_stories.md
@@ -0,0 +1,15 @@
+# Käyttäjätarinat/User stories
+
+* Admin voi lisätä otteluita, ja muokata kertoimia
+
+* Vaihtoehtoisesti ottelulista saadaan jostain muualta, ja vain kertoimien määritys pitää tehdä
+
+* Admin voi liittää otteluihin vetokohteita (Betting_offer)
+
+* Pelaaja voi rekisteröityä ja sen jälkeen lyödä vetoa liittämällä vetoon (Bet) yhden tai useamman vetokohteen (Betting_offer)
+
+* Vedon ja vetokohteen muodostama yhteys yksilöidään Betting_offer_of_bet-taulussa
+
+* Pelaaja voi tarkastella vetohistoriaansa
+
+* Admin voi tarkastella kaikki vetoja ja vetokohteiden vaihtoja 
