146a038f610bc4c90a16bac0effced346e456c6a
Juuso Silvennoinen <32332926+Jsos17@users.noreply.github.com>
2018-06-11 21:52:55 +0300

    Unbreak it back

--- application/betting_offers/models.py
+++ application/betting_offers/models.py
@@ -33,7 +33,7 @@ class Betting_offer(db.Model):
         stmt = text("SELECT sport_match.home, sport_match.away, sport_match.id, betting_offer.id, \
                      COUNT(bet_coupon.id), SUM(bet_coupon.stake_eur), SUM(bet_coupon.stake_cent), sport_match.start_time \
                      FROM sport_match, betting_offer, bet_coupon, betting_offer_of_coupon WHERE betting_offer.match_id = sport_match.id \
-                     AND betting_offer_of_coupon.betting_offer_id = betting_offer.id AND betting_offer_of_coupon.bet_coupon_id = bet_coupon.id GROUP BY sport_match.id")
+                     AND betting_offer_of_coupon.betting_offer_id = betting_offer.id AND betting_offer_of_coupon.bet_coupon_id = bet_coupon.id GROUP BY sport_match.id, betting_offer.id")
 
         res = db.engine.execute(stmt)
 
