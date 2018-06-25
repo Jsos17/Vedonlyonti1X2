0b4e5150aa55a11d18c4081ea6fab6627338bdf5
Juuso Silvennoinen <32332926+Jsos17@users.noreply.github.com>
2018-06-25 20:48:08 +0300

    Lomakkeiden validoinnin viestien oikein kohdistamista/validoinnin pysäytys

--- application/bet_coupons/forms.py
+++ application/bet_coupons/forms.py
@@ -6,9 +6,9 @@ def validate_min_max_stake(form, field):
     try:
         stake = int(100 * form.stake.data)
         if stake > float(form.max_stake.data) * 100:
-            raise validators.ValidationError("Your stake is larger than the smallest maximum stake of the selected offers " + form.max_stake.data + " eur")
+            raise validators.StopValidation("Your stake is larger than the smallest maximum stake of the selected offers " + form.max_stake.data + " eur")
     except (TypeError, ValueError):
-        raise validators.ValidationError("Please, select a stake")
+        raise validators.StopValidation("Please, select a stake")
 
 def validate_balance(form, field):
     stake_cents = 0
@@ -31,7 +31,7 @@ def validate_stake_places(form, field):
     if balance_str.find(".") != -1:
         split_eur_cent = balance_str.split(".")
         if len(split_eur_cent[1]) > 2:
-            raise validators.ValidationError("Only two decimal places allowed")
+            raise validators.StopValidation("Only two decimal places allowed")
 
 def validate_acceptable_range_eur(form, field):
     try:
